7624613ab1718d8a625f4e0bd61940232c22a80a
Dennis Savchenko <savchenko.denis007@gmail.com>
2024-03-10 22:29:12 +0100

    If statement was added

--- Project.java
+++ Project.java
@@ -1,5 +1,8 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int i = 1;
+        if(i == 1) System.out.println("Bye");
+        else System.out.println("Hi");
     }
 }
